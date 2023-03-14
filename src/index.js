fe5ebe7b509f96e240c7a0b26ab5d5ea27771bc2
arturMucci <artur.mucci@gmail.com>
2023-03-14 17:55:20 -0300

    requisito 01 - done

--- src/index.js
+++ src/index.js
@@ -1,16 +1,24 @@
 const express = require('express');
+const fs = require('fs').promises;
 
 const app = express();
 app.use(express.json());
 
 const HTTP_OK_STATUS = 200;
 const PORT = process.env.PORT || '3001';
-// Abrindo PR.
+
+async function readFile() {
+  const data = await fs.readFile('src/talker.json', 'utf-8');
+  const retorno = JSON.parse(data);
+  return retorno;
+}
+
 // não remova esse endpoint, e para o avaliador funcionar
-app.get('/', (_request, response) => {
-  response.status(HTTP_OK_STATUS).send();
+app.get('/', (_request, response) => response.status(HTTP_OK_STATUS).send());
+
+app.get('/talker', async (_request, response) => {
+  const data = await readFile();
+  return response.status(HTTP_OK_STATUS).json(data);
 });
 
-app.listen(PORT, () => {
-  console.log('Online');
-});
+app.listen(PORT, () => console.log('Online'));
