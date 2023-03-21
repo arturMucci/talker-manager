5fc0098f30be051931c939ee8841bb2c9cadd493
arturMucci <artur.mucci@gmail.com>
2023-03-21 11:44:15 -0300

    requisito 09 e 10 - refatoração

--- src/middlewares/validation/validateTalker/validateSearchDate.js
+++ src/middlewares/validation/validateTalker/validateSearchDate.js
@@ -1,14 +1,13 @@
 const { HTTP_BADREQUEST_STATUS } = require('../../../serverStatus/index');
 
-const message = {
-  watchedAtNaD: { message: 'O parâmetro "date" deve ter o formato "dd/mm/aaaa"' },
-};
-
 function validateSearchDate(req, res, next) {
   const { date } = req.query;
   const dateRegex = /^([0-2][0-9]|(3)[0-1])(\/)(((0)[0-9])|((1)[0-2]))(\/)\d{4}$/i;
+
   if (date && !date.match(dateRegex)) {
-    return res.status(HTTP_BADREQUEST_STATUS).json(message.watchedAtNaD);
+    return res
+      .status(HTTP_BADREQUEST_STATUS)
+      .json({ message: 'O parâmetro "date" deve ter o formato "dd/mm/aaaa"' });
   }
   return next();
 }
--- src/middlewares/validation/validateTalker/validateSearchRate.js
+++ src/middlewares/validation/validateTalker/validateSearchRate.js
@@ -1,14 +1,11 @@
 const { HTTP_BADREQUEST_STATUS } = require('../../../serverStatus/index');
 
-const message = {
-  notRate: { message: 'O campo "rate" é obrigatório' },
-  rateNotValid: { message: 'O campo "rate" deve ser um número inteiro entre 1 e 5' },
-};
-
 function validateSearchRate(req, res, next) {
   const { rate } = req.query;
   if (rate && !(Number.isInteger(+rate) && +rate >= 1 && +rate <= 5)) {
-    return res.status(HTTP_BADREQUEST_STATUS).json(message.rateNotValid);
+    return res
+      .status(HTTP_BADREQUEST_STATUS)
+      .json({ message: 'O campo "rate" deve ser um número inteiro entre 1 e 5' });
   }
   return next();
 }
